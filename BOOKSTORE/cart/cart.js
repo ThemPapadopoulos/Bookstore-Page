dd09f24219f508d7e215704ef3a414c806a896fc
themiskiller <them.papadopoulos@gmail.com>
2020-12-03 16:21:28 +0200

    bookstore project

---
+++ BOOKSTORE/cart/cart.js
@@ -0,0 +1,8 @@
+function openNav() {
+      document.getElementsByClassName('overlay')[0].style.width ="100%";
+  }
+  
+  
+  function closeNav() {
+      document.getElementsByClassName('overlay')[0].style.width ="0%";
+  }
